79f55bd81a46472be9507f5a76ed6599902f2a8a
Teodora Gasovic <teodora.gasovic@gmail.com>
2025-09-19 16:16:33 +0200

    Initial setup..

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.se201_bookmanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.se201_bookmanager to javafx.fxml;
+    exports com.example.se201_bookmanager;
+}
